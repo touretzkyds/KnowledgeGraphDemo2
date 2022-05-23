3421a7c58d65a41a14183088ab978efda2d4fd6c
Arthur Barelli <tuca@arthurs-air.lan>
2022-05-23 14:29:28 -0400

    Removing needless items

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.logging.Logger;
-
-public class Test {
-  static Logger log = Logger.getLogger(Test.class.getName());
-
-  public static void init() {
-    log.info("Test is loaded");
-  }
-}
--- node_modules/@rdfjs/prefix-map/test/support/exampleData.js
+++
@@ -1,9 +0,0 @@
-import rdf from '@rdfjs/data-model'
-
-const example = {
-  prefix: rdf.namedNode('http://example.org/'),
-  prefix1: rdf.namedNode('http://example.org/1/'),
-  prefix2: rdf.namedNode('http://example.org/2/')
-}
-
-export default example
